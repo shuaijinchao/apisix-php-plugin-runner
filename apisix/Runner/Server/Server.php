d54ae4a9b0851d1747499a16d59c944f2cf7bc59
Janko <shuaijinchao@gmail.com>
2021-10-23 18:29:23 +0800

    feat: add default plugin template

--- apisix/Runner/Server/Server.php
+++ apisix/Runner/Server/Server.php
@@ -1,10 +1,29 @@
 <?php
 
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
+
 namespace APISIX\Runner\Server;
 
 class Server
 {
-
     /**
      * @var Config
      */
@@ -12,6 +31,10 @@ class Server
 
     protected $sock;
 
+    /**
+     * Server constructor.
+     * @param Config $config
+     */
     public function __construct(Config $config)
     {
         $this->config = $config;
@@ -46,16 +69,29 @@ class Server
         do {
             $conn = socket_accept($this->sock);
             if (!is_resource($conn)) {
-                die("ERR: socket_accept failed, reason: " . socket_strerror(socket_last_error($this->sock)));
+                die("ERR: socket_accept failed, reason: " . socket_strerror(socket_last_error($conn)));
             }
-            // reading protocol header
+
+            // reading request header
             $buffer = socket_read($conn, 4);
             if (!$buffer) {
                 continue;
             }
-            $bytes = unpack("Sty/nlength", $buffer);
-            dump($bytes["ty"]);
-            dump($bytes["length"]);
+
+            // parsing request header
+            $bytes = unpack("sty/nlen", $buffer);
+            print("request type: " . $bytes["ty"] . " len: " . $bytes["length"] . "\n");
+
+            // reading request data
+            $buffer = socket_read($conn, $bytes["len"]);
+            if (!$buffer) {
+                print("ERR: socket_read failed, reason: " . socket_strerror(socket_last_error($conn)) . "\n");
+                continue;
+            }
+
+            // processing request
+            $handler = Handler::init($bytes["ty"], $buffer);
+            $handler->dispatch();
         } while (true);
     }
 
